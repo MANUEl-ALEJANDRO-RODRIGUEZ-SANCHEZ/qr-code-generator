4787701cef54780aff4239b0773e58936ef4e40f
Manuel Alejandro Rodriguez Sanchez <manuel.rodriguez9120@alumnos.udg.mx>
2024-10-09 22:59:36 -0600

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    devServer: {
-        host: '0.0.0.0',
-        disableHostCheck: true,
-        port: 10000,
-    },
     images: {
         domains: ['api.qrserver.com'], // Reemplaza 'example.com' con el dominio de las imágenes externas
     },
